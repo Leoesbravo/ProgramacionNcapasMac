4bdf3216579f96181005b2c3ffb61a4d3bfb766f
macbookmba12 <lescogidobravo@outlook.com>
2023-08-17 11:28:56 -0600

    carrito

---
+++ ML/Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ML
+{
+	public class Venta
+	{
+		public Venta()
+		{
+		}
+		public decimal  Total { get; set; }
+		public int  Cantidad { get; set; }
+		public decimal SubTotal { get; set; }
+		public List<object> Carrito { get; set; }
+
+	}
+}
+
--- PL/Controllers/VentaController.cs
+++ PL/Controllers/VentaController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -19,6 +20,35 @@ namespace PL.Controllers
             ML.Result result = BL.Producto.GetAllEF(producto);
             producto.Productos = result.Objects;
             return View(producto);
+        }
+        public ActionResult AddCarrito(int idProducto)
+        {
+            if(HttpContext.Session.GetString("Carrito") == null)
+            {
+                ML.Venta carrito = new ML.Venta();
+                carrito.Carrito = new List<object>();
+                ML.Result result = BL.Producto.GetByIdEF(idProducto);
+                if (result.Correct)
+                { 
+                    ML.Producto producto = (ML.Producto)result.Object;
+                    carrito.Carrito.Add(producto);
+                    //serializar carrito
+                    HttpContext.Session.SetString("Carrito", Newtonsoft.Json.JsonConvert.SerializeObject(carrito.ToString()));
+                }
+            
+            }
+            else
+            {
+                //deserialiar la sesion
+               ML.Venta venta = HttpContext.Session.GetString("Carrito");
+                //agregar el nuevo producto, tienen modificar el total
+            }
+
+            return RedirectToAction("Catalogo");
+        }
+        public ActionResult GetCarrito()
+        {
+
         }
     }
 }
