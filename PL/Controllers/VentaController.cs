91cde7a852a8d2170605cd8446a7745882767d49
macbookmba12 <lescogidobravo@outlook.com>
2023-08-28 16:24:24 -0600

    mapa

--- PL/Controllers/VentaController.cs
+++ PL/Controllers/VentaController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// For more information on enabling MV C for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PL.Controllers
 {
